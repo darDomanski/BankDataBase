b710717db47d921de3277bab5a30b4c258ac8213
Adelaiten <karoltrzaska19@gmail.com>
2019-03-05 14:01:33 +0100

    added not null constraint to amount column in balances

--- balances.sql
+++ balances.sql
@@ -1,6 +1,6 @@
 CREATE TABLE balances (
   id SERIAL PRIMARY KEY,
   currency_id INTEGER,
-  amount DECIMAL(9,2),
+  amount DECIMAL(9,2) NOT NULL,
   FOREIGN KEY (currency_id) REFERENCES currencies(id)
 );
