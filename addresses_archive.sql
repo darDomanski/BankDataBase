87d52f27fbdf5a8cb4c42fc67d6e4fc3eca6e9a8
darDomanski <dardomanski@gmail.com>
2019-03-05 15:33:11 +0100

    Add forgotten 'not null'

--- addresses_archive.sql
+++ addresses_archive.sql
@@ -4,6 +4,6 @@ create table if not exists addresses_archive
   street   varchar(50)        not null,
   zip_code varchar(15)        not null,
   city     varchar(80)        not null,
-  country  varchar(100)
+  country  varchar(100)       not null
 )
 ;
