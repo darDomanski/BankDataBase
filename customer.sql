8398256c6e13f5e11ccc592cde4b37a55d9c893c
darDomanski <dardomanski@gmail.com>
2019-03-05 13:29:06 +0100

    Add birth date to customers table

--- customer.sql
+++ customer.sql
@@ -1,9 +1,10 @@
 CREATE TABLE customers
 (
   id               SERIAL PRIMARY KEY NOT NULL,
-  address_id       int                NOT NULL references addresses.id,
   customer_name    VARCHAR(50)        NOT NULL,
   customer_surname VARCHAR(50)        NOT NULL,
+  birth_date       DATE               NOT NULL,
+  address_id       int                NOT NULL references addresses.id,
   phone            INT                NOT NULL,
   email            VARCHAR(50)        NOT NULL,
   sex_id           INT                NOT NULL references genders.id,
