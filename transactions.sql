ad426d7f12d02af55a293ad196ee5a083697f9c4
Adelaiten <karoltrzaska19@gmail.com>
2019-03-05 14:00:21 +0100

    added not null constraint and default constraint to transactions table create query

--- transactions.sql
+++ transactions.sql
@@ -1,10 +1,10 @@
 CREATE TABLE transactions (
   id INTEGER PRIMARY KEY,
   account_id INTEGER,
-  account_number INTEGER,
-  change DECIMAL(9,2),
-  type VARCHAR(50),
-  description VARCHAR(100),
-  date DATE,
+  account_number INTEGER NOT NULL,
+  change DECIMAL(9,2) NOT NULL,
+  type VARCHAR(50) NOT NULL,
+  description VARCHAR(100) DEFAULT '',
+  date DATE NOT NULL,
   FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
