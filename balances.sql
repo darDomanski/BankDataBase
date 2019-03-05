bddc4e5d5e7f2a51a3cce9c41432eb1592a1a3dc
Adelaiten <karoltrzaska19@gmail.com>
2019-03-05 13:53:48 +0100

    fixed balances creation table query - foreign key to accounts redundant and deleted

--- balances.sql
+++ balances.sql
@@ -3,6 +3,5 @@ CREATE TABLE balances (
   account_id INTEGER,
   currency_id INTEGER,
   amount DECIMAL(9,2),
-  FOREIGN KEY (account_id) REFERENCES accounts(id),
   FOREIGN KEY (currency_id) REFERENCES currencies(id)
 );
