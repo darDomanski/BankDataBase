ba9c15828527e46c9d1bebe1d5809e3665089acd
darDomanski <dardomanski@gmail.com>
2019-03-06 15:26:45 +0100

    Add query to copy data from csv file

--- customer.sql
+++ customer.sql
@@ -5,12 +5,14 @@ CREATE TABLE if not exists customers
   customer_surname VARCHAR(50)        NOT NULL,
   birth_date       DATE               NOT NULL,
   address_id       int                NOT NULL references addresses (id),
-  phone            INT                NOT NULL,
+  phone            BIGINT             NOT NULL,
   email            VARCHAR(50)        NOT NULL,
   sex_id           INT                NOT NULL references genders (id),
   pesel            VARCHAR(11)        NOT NULL,
   start_date       DATE               NOT NULL
 );
+
+drop table customers cascade;
 
 CREATE OR REPLACE FUNCTION copy_customer_to_archive() returns trigger as
 $$
@@ -29,3 +31,5 @@ CREATE TRIGGER moveDeleted
   ON customers
   FOR EACH ROW
 EXECUTE PROCEDURE copy_customer_to_archive();
+
+COPY customers from '/home/darski/codecool/advanced/tw2/BankDataBase/data_sources/customers.csv' delimiter ',';
