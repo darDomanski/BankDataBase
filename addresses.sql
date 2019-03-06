95e9b14246453da4df781a9ca999dbf8590f91fd
darDomanski <dardomanski@gmail.com>
2019-03-06 11:13:56 +0100

    Add query to create trigger which moves deleted address to archive

--- addresses.sql
+++ addresses.sql
@@ -7,3 +7,18 @@ create table if not exists addresses
   country  varchar(100)
 )
 ;
+
+CREATE OR REPLACE FUNCTION copy_address_to_archive() returns trigger as
+$$
+BEGIN
+  INSERT into addresses_archive (street, zip_code, city, country)
+  VALUES (old.street, old.zip_code, old.city, old.country);
+  return null;
+end;
+$$ language plpgsql;
+
+CREATE TRIGGER moveDeletedAddress
+  AFTER DELETE
+  ON addresses
+  FOR EACH ROW
+EXECUTE PROCEDURE copy_address_to_archive();
