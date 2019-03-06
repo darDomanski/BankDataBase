8897212fe3af1576da51db6dc950560014258d08
Adelaiten <karoltrzaska19@gmail.com>
2019-03-06 11:11:09 +0100

    transactions create table query changed id to serial, check if account number exist function

--- transactions.sql
+++ transactions.sql
@@ -1,10 +1,22 @@
 CREATE TABLE transactions (
-  id INTEGER PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   account_id INTEGER,
   account_number INTEGER NOT NULL,
   change DECIMAL(9,2) NOT NULL,
   type VARCHAR(50) NOT NULL,
   description VARCHAR(100) DEFAULT '',
   date DATE NOT NULL,
-  FOREIGN KEY (account_id) REFERENCES accounts(id)
+  FOREIGN KEY (account_id) REFERENCES accounts(id),
+  CHECK (check_account_number_exist(account_number))
 );
+
+
+CREATE OR REPLACE FUNCTION check_account_number_exist(number INTEGER) RETURNS BOOLEAN AS $$
+  BEGIN
+  IF EXISTS(SELECT account_number FROM accounts WHERE account_number = number) THEN
+    RETURN TRUE;
+    ELSE
+    RETURN FALSE;
+  end if;
+  end;
+  $$ LANGUAGE plpgsql;
