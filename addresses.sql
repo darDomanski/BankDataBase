3d5f7f95613f177e7d54ee516f0d6b5e8400de66
Adelaiten <karoltrzaska19@gmail.com>
2019-03-06 17:36:17 +0100

    addresses

--- addresses.sql
+++ addresses.sql
@@ -23,3 +23,5 @@ CREATE TRIGGER moveDeletedAddress
   FOR EACH ROW
 EXECUTE PROCEDURE copy_address_to_archive();
 
+COPY addresses FROM '/home/karol/codecool/Codecool-advanced-module/II TW-Week/BankDataBase/addresses.csv' DELIMITER ',';
+SELECT * FROM addresses;
